69b4231e9589a4e5876494a1e9d9ae61199b248e
Ryan Camp <campryan@comcast.net>
2021-07-02 12:51:01 -0600

    Set up has many items validation for merchant model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Merchant do
   describe 'validations' do
-    it {should validate_presence_of :name}
+    it { should validate_presence_of :name }
   end
 
   describe 'relationships' do
-
+    it { should have_many :items }
   end
 
   describe 'methods' do
