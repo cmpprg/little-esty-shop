46c4a0a70db48a9aea4d57f1dd5696ecea3c7799
Ryan Camp <campryan@comcast.net>
2021-07-02 11:14:58 -0600

    Create model test for Merchant

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Merchant do
+  describe 'validations' do
+    it {should validate_presence_of :name}
+  end
+
+  describe 'relationships' do
+
+  end
+
+  describe 'methods' do
+
+  end
+end
