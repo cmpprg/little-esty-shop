693a298c5e9fe9874f0238d23a32850b59c0c0a6
Ryan Camp <campryan@comcast.net>
2021-07-02 15:57:58 -0600

    Add rake task for loading transactions csv data to db

--- db/migrate/20210702213920_create_transaction.rb
+++ db/migrate/20210702213920_create_transaction.rb
@@ -3,7 +3,7 @@ class CreateTransaction < ActiveRecord::Migration[5.2]
     create_table :transactions do |t|
       t.references :invoice, foreign_key: true
       t.bigint :credit_card_number
-      t.datetime :credit_card_expiration
+      t.datetime :credit_card_expiration_date
       t.integer :result
 
       t.timestamps
