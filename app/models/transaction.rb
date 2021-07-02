47e3d7490611e230acfdd58d06c5fd0d9457ad63
Ryan Camp <campryan@comcast.net>
2021-07-02 15:50:16 -0600

    Set up validations for transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,14 @@
 class Transaction < ApplicationRecord
-  
+  belongs_to :invoice
+
+  validates :credit_card_number, {
+    presence: true,
+    numericality: { integer_only: true }
+  }
+
+  validates :result, {
+    presence: true,
+  }
+
+  enum result: [:failed, :success]
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Transaction do
   describe 'validations' do
     it { should validate_presence_of :credit_card_number }
+    it { should validate_numericality_of :credit_card_number }
     it { should validate_presence_of :result }
     it { should define_enum_for :result }
   end
