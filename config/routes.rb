7f9d3131f9e5de306a714ccacbb6b301b20004ff
Ryan Camp <campryan@comcast.net>
2021-07-03 19:12:46 -0600

    Setup dashboard controller for merchant dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :mechants, only: [] do 
+  resources :merchants, only: [] do 
     resources :dashboard, only: [ :index ]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
