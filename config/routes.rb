b4430e2f54fe8a6b38ee8b1a40e0261f1f5d7148
Ryan Camp <campryan@comcast.net>
2021-07-04 18:20:33 -0600

    Add nested routes for merchant items and invoices index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   scope module: 'merchant' do
     resources :merchants, only: [] do 
       resources :dashboard, only: [ :index ]
+      resources :items, only: [ :index ]
+      resources :invoices, only: [ :index ]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
