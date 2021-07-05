747a56641d9f15989802bfc4d7fbe309b5a4f48a
Ryan Camp <campryan@comcast.net>
2021-07-04 18:17:23 -0600

    Write spec to check merchant items and invoices links on template

--- spec/features/merchant/dashboard/index_spec.rb
+++ spec/features/merchant/dashboard/index_spec.rb
@@ -4,16 +4,36 @@ RSpec.describe 'As a merchant.', type: :feature do
   describe 'When I visit my dashboard.' do 
     it 'I can see my name displayed.' do
       merchant = create(:merchant)
-
-      visit "/merchants/#{merchant.id}/dashboard"
-
+      
+      visit "/merchants/#{merchant.id}/dashboard" #merchant_dashboard_index_path
+      
       within '#merchant-name' do
         expect(page).to have_content(merchant.name)
       end
     end
+    
+    it 'I can see and use a link to merchant items index' do
+      merchant = create(:merchant)
 
-    xit 'I can see links to both merchant items index and merchant invoices index' do
-      require 'pry';binding.pry
+      visit(merchant_dashboard_index_path(merchant))
+
+      within '.dashboard-links' do
+        click_link('My Items')
+      end
+
+      expect(current_path).to eq("/merchants/#{merchant.id}/items")
+    end
+
+    it 'I can see and use a link to merchant invoices index' do
+      merchant = create(:merchant)
+
+      visit(merchant_dashboard_index_path(merchant))
+
+      within '.dashboard-links' do
+        click_link('My Invoices')
+      end
+
+      expect(current_path).to eq("/merchants/#{merchant.id}/invoices")
     end
   end
 end
