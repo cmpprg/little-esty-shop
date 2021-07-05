c6e9887b95fbaf2120149b0b68ce08a64a7637f5
Ryan Camp <campryan@comcast.net>
2021-07-04 18:32:35 -0600

    Add merchant scoped items and invoices controllers with index actions

---
+++ app/controllers/merchant/invoices_controller.rb
@@ -0,0 +1,5 @@
+class Merchant::InvoicesController < Merchant::BaseController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
       resources :invoices, only: [ :index ]
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
