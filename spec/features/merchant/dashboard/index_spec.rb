e1d46c328315535836f8db845b15a8072fb85b77
Ryan Camp <campryan@comcast.net>
2021-07-09 08:03:11 -0600

    Write test for top 5 customers on merchant dashboard

--- spec/features/merchant/dashboard/index_spec.rb
+++ spec/features/merchant/dashboard/index_spec.rb
@@ -35,5 +35,52 @@ RSpec.describe 'As a merchant.', type: :feature do
 
       expect(current_path).to eq("/merchants/#{merchant.id}/invoices")
     end
+
+    it 'I see the names of the top 5 customers, who have the largest number of
+        successful transactions with me.' do
+      
+      merchant = create(:merchant)
+      customer_1 = create(:customer)
+      customer_2 = create(:customer)
+      customer_3 = create(:customer)
+      customer_4 = create(:customer)
+      customer_5 = create(:customer)
+      customer_6 = create(:customer)
+      customer_7 = create(:customer)
+
+      def create_necessary_resources(merchant, customer, result)
+        build(:invoice_item) do |ii|
+          ii.invoice.customer = customer
+          ii.item.merchant = merchant
+          create(:transaction, invoice: ii.invoice, result: result)
+        end.save
+      end
+      # Create resources from transactions to merchants, switch customer 1 & 2
+      # to ensure order is based on number of successful transactions and not
+      # order in DB.
+      7.times { create_necessary_resources(merchant, customer_2, 'success') }
+      6.times { create_necessary_resources(merchant, customer_1, 'success') }
+      5.times { create_necessary_resources(merchant, customer_3, 'success') }
+      4.times { create_necessary_resources(merchant, customer_4, 'success') }
+      3.times { create_necessary_resources(merchant, customer_5, 'success') }
+      2.times { create_necessary_resources(merchant, customer_6, 'success') }
+      1.times { create_necessary_resources(merchant, customer_7, 'success') }
+      # I want to ensure that no failed transactions are counted
+      2.times do 
+        create_necessary_resources(merchant, customer_1, 'failed')
+        create_necessary_resources(merchant, customer_2, 'failed')
+        create_necessary_resources(merchant, customer_3, 'failed')
+        create_necessary_resources(merchant, customer_4, 'failed')
+        create_necessary_resources(merchant, customer_5, 'failed')
+      end
+
+      within('#top-five-customers') do
+        expect(page.all('.top-customer')[0]).to have_content("1. #{customer_2.first_name} #{customer_2.last_name} - 7 purchases")
+        expect(page.all('.top-customer')[1]).to have_content("2. #{customer_1.first_name} #{customer_1.last_name} - 6 purchases")
+        expect(page.all('.top-customer')[2]).to have_content("3. #{customer_3.first_name} #{customer_3.last_name} - 5 purchases")
+        expect(page.all('.top-customer')[3]).to have_content("4. #{customer_4.first_name} #{customer_4.last_name} - 4 purchases")
+        expect(page.all('.top-customer')[4]).to have_content("5. #{customer_5.first_name} #{customer_5.last_name} - 3 purchases")
+      end
+    end
   end
 end
