ea0fb0309a346bf40d9a2e9ff977a4dd6aa3e078
Ryan Camp <campryan@comcast.net>
2021-07-14 09:47:16 -0600

    Refactor #get_top_five_customers query

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -10,8 +10,8 @@ private
   def get_top_five_customers(merchant)
     tfc = Customer.joins(invoices: [:transactions, invoice_items: {item: :merchant}])
     tfc = tfc.where('transactions.result = 1', 'merchants.id = ?', merchant)
-    tfc = tfc.select('customers.first_name || \' \' || customers.last_name as full_name, COUNT(transactions.id) as quantity_successful_transactions')
-    tfc = tfc.group('full_name').order('quantity_successful_transactions DESC').limit(5)
+    tfc = tfc.select('customers.* , COUNT(transactions.id) as quantity_successful_transactions')
+    tfc = tfc.group('customers.id').order('quantity_successful_transactions DESC').limit(5)
     # require 'pry';binding.pry
   end
 end
