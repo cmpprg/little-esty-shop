9b92ffe31f37d896e4780f040a0934789d32d808
Ryan Camp <campryan@comcast.net>
2021-07-14 12:27:15 -0600

    Refactor merchant dashboard to use model method for top_five_customer

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -2,12 +2,5 @@ class Merchant::DashboardController < Merchant::BaseController
 
   def index
     @merchant = Merchant.find_by_id(params[:merchant_id])
-    @top_five_customers = get_top_five_customers(@merchant)
-  end
-
-private
-
-  def get_top_five_customers(merchant)
-    merchant.top_five_customers
   end
 end
