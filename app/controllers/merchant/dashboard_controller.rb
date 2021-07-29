7aeba31afa2ab647c2e82140b82ae1a2302c5c63
Ryan Camp <campryan@comcast.net>
2021-07-28 21:31:12 -0600

    Implement functionality to find and display items ready to ship

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -2,5 +2,13 @@ class Merchant::DashboardController < Merchant::BaseController
 
   def index
     @merchant = Merchant.find_by_id(params[:merchant_id])
+    @items_ready_to_ship = items_ready_to_ship
   end
+
+  private
+
+    def items_ready_to_ship
+      @merchant.items.joins(invoice_items: :invoice)
+      .where.not(invoice_items: { status: 'shipped' })
+    end
 end
