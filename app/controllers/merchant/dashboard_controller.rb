4d9e6d493e5dd4c7be17e9fef79b86f8fc17bc23
Ryan Camp <campryan@comcast.net>
2021-07-29 11:18:34 -0600

    Move items ready to ship logic from controller to merchant model

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,14 +1,5 @@
 class Merchant::DashboardController < Merchant::BaseController
-
   def index
     @merchant = Merchant.find_by_id(params[:merchant_id])
-    @items_ready_to_ship = items_ready_to_ship
   end
-
-  private
-
-    def items_ready_to_ship
-      @merchant.items.joins(invoice_items: :invoice)
-      .where.not(invoice_items: { status: 'shipped' })
-    end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -11,4 +11,9 @@ class Merchant < ApplicationRecord
     tfc = tfc.select('customers.* , COUNT(transactions.id) as quantity_successful_transactions')
     tfc = tfc.group('customers.id').order('quantity_successful_transactions DESC').limit(5)
   end
+
+  def items_ready_to_ship
+    self.items.joins(invoice_items: :invoice)
+    .where.not(invoice_items: { status: 'shipped' })
+  end
 end
